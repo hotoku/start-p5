22bcf07b9675fc68abc394d2e248059ce45037b2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-07 05:37:01 +0900

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -1,1 +1,8 @@
-console.log("index.bundle.js");
+var myIndex;
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+console.log("index.ts");
+
+myIndex = __webpack_exports__;
+/******/ })()
+;
