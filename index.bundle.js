9c52ee3eab2e6bdf6832cff758b0a7ea97f3a0ce
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-07 04:43:53 +0900

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -11,9 +11,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var matter_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(matter_js__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var p5__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(35);
 /* harmony import */ var p5__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(p5__WEBPACK_IMPORTED_MODULE_1__);
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 
 
-console.log("index.ts");
 function sketch(p) {
     let engine;
     let world;
@@ -89,7 +97,15 @@ function sketch(p) {
         }
     };
 }
-new (p5__WEBPACK_IMPORTED_MODULE_1___default())(sketch);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        addEventListener("load", () => {
+            new (p5__WEBPACK_IMPORTED_MODULE_1___default())(sketch);
+            console.log("event listener");
+        });
+    });
+}
+main();
 
 
 /***/ })
